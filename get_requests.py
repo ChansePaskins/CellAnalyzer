225609f6c939f5985d3739a27eec577b7c186d84
ChansePaskins <149602257+ChansePaskins@users.noreply.github.com>
2024-06-26 16:25:22 -0600

    changed docx to pdf display

--- get_requests.py
+++ get_requests.py
@@ -1,11 +1,12 @@
-import docx
 import requests
 
 
-def read_docx_from_github(url):
+def read_pdf_from_github(url):
     response = requests.get(url)
-    with open("Cell Analysis Website SOP.docx", "wb") as file:
-        file.write(response.content)
-    doc = docx.Document("Cell Analysis Website SOP.docx")
-    return doc
+    if response.status_code == 200:
+        temp_file_path = "temp_sop.pdf"
+        with open(temp_file_path, "wb") as file:
+            file.write(response.content)
+        return temp_file_path
+
 
--- pages/3_Standard_Operating_Procedure.py
+++ pages/3_Standard_Operating_Procedure.py
@@ -1,13 +1,26 @@
 import streamlit as st
-import docx
-from get_requests import read_docx_from_github
+from get_requests import read_pdf_from_github
+import fitz
 
 # Page styling and title
 st.set_page_config(layout="wide")
 st.title("Cell Counter and Area Analyzer Pipeline")
 
-url = "https://github.com/ChansePaskins/CellAnalyzer/raw/main/Cell%20Analysis%20Website%20SOP.docx"
-doc = read_docx_from_github(url)
 
-for para in doc.paragraphs:
-    st.write(para.text)
+def display_pdf(file_path):
+    doc = fitz.open(file_path)
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
+        text = page.get_text("text")
+        st.write(text)
+        images = page.get_images(full=True)
+        for img_index, img in enumerate(images):
+            xref = img[0]
+            base_image = doc.extract_image(xref)
+            image_bytes = base_image["image"]
+            st.image(image_bytes)
+
+
+url = "https://github.com/ChansePaskins/CellAnalyzer/raw/main/Cell%20Analysis%20Website%20SOP.pdf"
+doc = read_pdf_from_github(url)
+display_pdf(doc)
