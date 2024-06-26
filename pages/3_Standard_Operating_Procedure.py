33a58e426153a42d015c8341b04a3048b9d3ce35
ChansePaskins <149602257+ChansePaskins@users.noreply.github.com>
2024-06-26 16:18:57 -0600

    Update 3_Standard_Operating_Procedure.py

--- pages/3_Standard_Operating_Procedure.py
+++ pages/3_Standard_Operating_Procedure.py
@@ -6,7 +6,7 @@ from get_requests import read_docx_from_github
 st.set_page_config(layout="wide")
 st.title("Cell Counter and Area Analyzer Pipeline")
 
-url = "https://github.com/ChansePaskins/CellAnalyzer/blob/1f512195797f716acd0a21600952326e62213267/Cell%20Analysis%20Website%20SOP.docx"
+url = "https://github.com/ChansePaskins/CellAnalyzer/raw/main/Cell%20Analysis%20Website%20SOP.docx"
 doc = read_docx_from_github(url)
 
 for para in doc.paragraphs:
